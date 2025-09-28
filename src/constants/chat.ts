54157317a7f26c7aa5b2d0b55760df63a8e30fc2
KeremSerinkan <keremserinkan20@gmail.com>
2025-09-28 17:27:19 +0300

    .

---
+++ src/constants/chat.ts
@@ -0,0 +1,3 @@
+
+export const SENT = "SENT"
+export const RECEIVED = "RECEIVED"
--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,17 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import React, { useState } from 'react'
 import AppHeader from '../components/AppHeader'
 import SentMessageCard from '../components/SentMessageCard'
 import ResponseMessageCard from '../components/ResponseMessageCard'
+import { s } from 'react-native-size-matters'
+import { RECEIVED, SENT } from '../constants/chat'
+
+interface Message{
+    id: number;
+    message: string;
+    type: string
+}
 
 const ChatScreen = () => {
-  return (
-    <View>
-      <AppHeader/>
-      <SentMessageCard message={"Selamun aleykum yapay zeka kardes"}/>
-      <ResponseMessageCard message={"Aleykum selam insan kardes"}/>
-    </View>
-  )
+    const messagesList : Message[] = [
+        {
+            message: "Selamun aleykum yapay zeka kardes",
+            id: 1,
+            type: SENT
+        },
+        {
+            message: "Aleykum selam insan kardes",
+            id: 2,
+            type: RECEIVED
+        },
+        {
+            message: "Keyfin yerinde mi",
+            id: 3,
+            type: SENT
+        }
+    ]
+    const [messages, setMessages] = useState<Message[]>(messagesList)
+
+    return (
+        <View>
+            <AppHeader />
+            <FlatList
+                data={messages}
+                keyExtractor={item => item.id?.toString()}
+                renderItem={({ item }) => {
+                    return item.type === SENT ?
+                        (<SentMessageCard message={item.message} />) :
+                        (<ResponseMessageCard message={item.message} />);
+                }}
+
+                contentContainerStyle={{ paddingHorizontal: s(8) }}
+            />
+
+        </View>
+    )
 }
 
 export default ChatScreen
